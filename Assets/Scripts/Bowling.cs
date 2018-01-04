0c1e4659f693c0e70291032e2dc7b6babcf35fe9
Gege Guo <satoxi@ftang.cn>
2018-01-04 13:38:04 +0800

    Move bowling

---
+++ Assets/Scripts/Bowling.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bowling : MonoBehaviour
+{
+	public void Move()
+	{
+		_rigidbody.AddForce(new Vector3(0, 0, 2000f));
+	}
+
+	[SerializeField]
+	private Rigidbody _rigidbody;
+}
---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+	private void Update()
+	{
+		HandleInput();
+	}
+
+	private void HandleInput()
+	{
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			MoveBowling();
+		}
+	}
+
+	private void MoveBowling()
+	{
+		_bowling.Move();
+	}
+
+	[SerializeField]
+	private Bowling _bowling;
+}
